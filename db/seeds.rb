67db13f3effa87cfb60fe9241a9f92e13eec7b6c
willashley23 <willashley23@gmail.com>
2016-08-31 11:16:23 -0700

    Basic design of event cards complete, not yet responsive.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@
   Event.create!({title: "2016 Sausalio Art Festival", price: 20, description: "art things", tag: "arts", image_url: "assets/art.png", date: DateTime.new(2016,9,3,17), location: "Sausalio, CA" })
   Event.create!({title: "Dent: Space Exploration Together", price: 10, description: "space", tag: "science", image_url: "assets/space.jpeg", date: DateTime.new(2016,11,1,17), location: "Oxnard, CA" })
   Event.create!({title: "San Francisco Comic Con", price: 150, description: "Comics!", tag:"comics", image_url: "assets/comicon.jpg", date: DateTime.new(2016,9,2,17), location: "San Francisco, CA" })
-  Event.create!({title: "Small Buisness Expo 2016", price: 35, description: "business", tag:"career", image_url: "assets/business.jpeg", date: DateTime.new(2016,10,15,17), location: "Oakland, CA" })
+  Event.create!({title: "Small Buisness Expo 2016", price: 35, description: "business", tag:"career", image_url: "assets/business.jpg", date: DateTime.new(2016,10,15,17), location: "Oakland, CA" })
--- frontend/components/event_index_item.jsx
+++ frontend/components/event_index_item.jsx
@@ -8,18 +8,20 @@ export default class EventIndexItem extends React.Component {
   render() {
 
     return (
+    <div className="flex-wrapper">
       <div className="event-card">
           <div className="event-card-main">
             <img src={this.props.event.image_url} className="event-image" />
-            <p>{this.props.event.date}</p>
-            <h3>{this.props.event.title}</h3>
-            <p>{this.props.event.location}</p>
+            <p className="event-date">{this.props.event.date}</p>
+            <h3 className="event-title">{this.props.event.title}</h3>
+            <p className="event-location">{this.props.event.location}</p>
           <div className="event-card-footer">
-            {this.props.event.tag}
+            <span className="event-tag">#{this.props.event.tag}</span>
             <img src= "assets/bookmark.png" className="bookmark-icon"/>
           </div>
           </div>
       </div>
+    </div>
     )
   }
 }
