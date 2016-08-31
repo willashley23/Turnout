ef6e2cd93327d3e7e7cbc3942833d3cced54682f
willashley23 <willashley23@gmail.com>
2016-08-30 17:59:39 -0700

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ config.public_file_server.enabled = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
