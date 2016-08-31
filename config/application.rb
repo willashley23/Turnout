d3272cd42568284dd513dbf36b18c436b7d7799d
willashley23 <willashley23@gmail.com>
2016-08-30 17:53:35 -0700

    change application css to .css.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Powwow
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
-
+    
   end
 end
