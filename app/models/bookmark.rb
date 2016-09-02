4b7eb7d7345acc315c423e4c3115b335bf01563a
willashley23 <willashley23@gmail.com>
2016-09-02 11:54:06 -0700

    Prepare bookmark model.

---
+++ app/models/bookmark.rb
@@ -0,0 +1,4 @@
+class Bookmark < ApplicationRecord
+  belongs_to :events
+  belongs_to :users
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902062036) do
+ActiveRecord::Schema.define(version: 20160902184059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
